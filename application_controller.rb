bd0f9e69970e438ce02f0fc0d25f8f8810fd9e6b
Patrick Huesler <patrick.huesler@gmail.com>
2010-05-10 20:23:11 +0200

    Registering ctrl+alt+command+d as Hotkey

--- application_controller.rb
+++ application_controller.rb
@@ -4,15 +4,26 @@
 # Created by phuesler on 10.05.10.
 # Copyright 2010 huesler informatik. All rights reserved.
 class ApplicationController
+  KEY_CODE_FOR_D = 2
+  
   attr_accessor :statusBarMenu
   
   def applicationDidFinishLaunching(notification)
     activateStatusMenu
+    registerHotkey
     @distributor = WindowDistributor.new
   end
   
   def distributeWindows(sender)
     @distributor.run
+  end
+  
+  def registerHotkey
+    c = DDHotKeyCenter.alloc.init
+    c.registerHotKeyWithKeyCode(KEY_CODE_FOR_D, modifierFlags:(NSControlKeyMask | NSAlternateKeyMask | NSCommandKeyMask),
+                                target:self,
+                                action: :"distributeWindows:",
+                                object:nil)
   end
   
   def activateStatusMenu
