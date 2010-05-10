3831d971a239d15ff5423e7aa24030e5f9cfdf56
Patrick Huesler <patrick.huesler@gmail.com>
2010-05-10 16:53:40 +0200

    Initial commit

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  WindowDistributor
+//
+//  Created by phuesler on 10.05.10.
+//  Copyright huesler informatik 2010. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
